b8ae3b0a7f9401de6bcd2f1f299ba3144c85e3ed
Jakub Mielniczuk <mielniczuk.jakub@gmail.com>
2019-05-07 14:55:24 +0200

    Adding CRC code to output text

--- src/com/teleinfgroup/ErrorDetectionAlgorithms/CRC.java
+++ src/com/teleinfgroup/ErrorDetectionAlgorithms/CRC.java
@@ -9,7 +9,7 @@ public class CRC extends ErrorDetectionAlgorithm {
 
     public CRC(CRCType type) {
         this.keyLength = type.getKeyLength();
-        this.polynomial = type.getPolynomial()<<(32-keyLength);
+        this.polynomial = type.getPolynomial() << (32 - keyLength);
     }
 
     public Integer getPolynomial() {
@@ -28,32 +28,47 @@ public class CRC extends ErrorDetectionAlgorithm {
         this.keyLength = keyLength;
     }
 
-
-    @Override
-    public int encodeMsg( String text) {
-        if(polynomial==-1 || keyLength<=0 || keyLength>32){
-            return -1;
-        }else
-        return computeCRC( text );
+    private StringBuilder stringToBinary(String text) {
+        byte[] bytes = text.getBytes();
+        StringBuilder binary = new StringBuilder();
+        for (byte b : bytes) {
+            int val = b;
+            for (int i = 0; i < 8; i++) {
+                binary.append((val & 128) == 0 ? 0 : 1);
+                val <<= 1;
+            }
+        }
+        return binary;
     }
 
-    private int computeCRC(String text){
-        int a = 0, b;
-        for (int i=0; i < text.length(); i++) {
-            b = text.charAt(i) << 24;
-            for (int j = 8;j>0; j-- ) {
+    @Override
+    public String encodeMsg(String text) {
+        if (polynomial == -1 || keyLength <= 0 || keyLength > 32) {
+            return null;
+        } else {
+            StringBuilder encodedMsg = stringToBinary(text);
+            encodedMsg.append(Integer.toHexString(computeCRC(text)));
+            return encodedMsg.toString();
+        }
+    }
 
-                if (((a^b) & (1<<31))>>>31==1) {
+    private int computeCRC(String text) {
+        int a = 0, b;
+        for (int i = 0; i < text.length(); i++) {
+            b = text.charAt(i) << 24;
+            for (int j = 8; j > 0; j--) {
+
+                if (((a ^ b) & (1 << 31)) >>> 31 == 1) {
                     a <<= 1;
                     b <<= 1;
                     a ^= polynomial;
-                }else {
+                } else {
                     a <<= 1;
                     b <<= 1;
                 }
             }
         }
-        return a>>>(32-keyLength);
+        return a >>> (32 - keyLength);
     }
 
     @Override
