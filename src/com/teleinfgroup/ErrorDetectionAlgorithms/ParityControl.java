5d7255cde5c239a5a904b2cec2779c0c2c9bd47d
Mateusz Nowosad <mateusz.nowosad1@gmail.com>
2019-05-08 19:37:20 +0200

    ParityControl class changed to interact with Message object.

--- src/com/teleinfgroup/ErrorDetectionAlgorithms/ParityControl.java
+++ src/com/teleinfgroup/ErrorDetectionAlgorithms/ParityControl.java
@@ -1,27 +1,36 @@
 package com.teleinfgroup.ErrorDetectionAlgorithms;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ParityControl extends ErrorDetectionAlgorithm {
 
     @Override
-    public String encodeMsg(String text) {
+    public void encodeMsg(Message message) {
+        String text=message.getMessageInBinary(8);
         boolean[] parityBits = new boolean[text.length()];
         int character;
+
+        Map<Integer, Byte> redundantData =new HashMap<>();
 
         StringBuilder encodedMsg = new StringBuilder(text);
 
         for (int i = 0; i < text.length(); i+=8) {
+            int offset = i+(i/8);
             character = Integer.parseInt(text.substring(i, i+8),2);
             //negatywna kontrola parzystości
             parityBits[i]= Integer.bitCount(character) % 2 == 1;
-            encodedMsg.insert(i+(i/8), parityBits[i] ? '0' : '1');
+            encodedMsg.insert(offset, parityBits[i] ? '0' : '1'); //position bits
+            redundantData.put(offset, parityBits[i] ? (byte)0 : (byte)1);
         }
-        return encodedMsg.toString();
+        message.setEncodedMessage(encodedMsg.toString()); //encoded
     }
 
     @Override
     public ArrayList<String> decodeMsg() {
+
+
         return null;
     }
 }
