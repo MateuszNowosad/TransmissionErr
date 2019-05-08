914c0e81101392c67f168c9707642634022f4917
Mateusz Nowosad <mateusz.nowosad1@gmail.com>
2019-05-08 12:34:13 +0200

    Main function updated with examples.

--- src/com/teleinfgroup/Main.java
+++ src/com/teleinfgroup/Main.java
@@ -3,6 +3,8 @@ package com.teleinfgroup;
 import com.teleinfgroup.ErrorDetectionAlgorithms.CRCType;
 import com.teleinfgroup.ErrorDetectionAlgorithms.ErrorDetectionAlgorithm;
 import com.teleinfgroup.ErrorDetectionAlgorithms.Hamming74;
+import com.teleinfgroup.ErrorDetectionAlgorithms.CRC;
+import com.teleinfgroup.ErrorDetectionAlgorithms.ParityControl;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,7 +25,57 @@ public class Main {
         CRCTypes.put("ATM", new CRCType("ATM", 0x7, 8));
     }
 
+    private static String stringToBinary(String text) {
+        byte[] bytes = text.getBytes();
+        StringBuilder binary = new StringBuilder();
+        for (byte b : bytes) {
+            int val = b;
+            for (int i = 0; i < 8; i++) {
+                binary.append((val & 128) == 0 ? 0 : 1);
+                val <<= 1;
+            }
+        }
+        return binary.toString();
+    }
+
+    private static String correctBinaryLength(int correctBlockSize, String text ){
+        if(text.length()<correctBlockSize){
+            text= "0".repeat(correctBlockSize-text.length()).concat(text);
+        }else {
+            int validBlockLength = text.length() % correctBlockSize;
+            if (validBlockLength != 0) {
+                text = "0".repeat(validBlockLength).concat(text);
+            }
+        }
+        return text;
+    }
+
     public static void main(String[] args) {
         init();
+
+        /**
+         * Wywołanie dla stringa ascii
+         * ErrorDetectionAlgorithm test = new Hamming74();
+         * System.out.println(hamming74.encodeMsg(correctBinaryLength(4,stringToBinary("ab"))));
+         *
+         * Wywołanie dla stringa binarnego
+         * ErrorDetectionAlgorithm test = new Hamming74();
+         * System.out.println(hamming74.encodeMsg(correctBinaryLength(4,"101")));
+         */
+        ErrorDetectionAlgorithm hamming74 = new Hamming74();
+        System.out.println("Hamming74 for ab: "+hamming74.encodeMsg(correctBinaryLength(4,stringToBinary("ab"))));
+        System.out.println("Hamming74 for 101: "+hamming74.encodeMsg(correctBinaryLength(4,"101")));
+
+        ErrorDetectionAlgorithm CRC16 = new CRC(CRCTypes.get("CRC-16"));
+        System.out.println("CRC16 for ab: "+CRC16.encodeMsg(correctBinaryLength(8,stringToBinary("ab"))));
+        System.out.println("CRC16 for 101: "+CRC16.encodeMsg(correctBinaryLength(8,"101")));
+
+        ErrorDetectionAlgorithm CRC32 = new CRC(CRCTypes.get("CRC-32"));
+        System.out.println("CRC32 for ab: "+CRC32.encodeMsg(correctBinaryLength(8,stringToBinary("ab"))));
+        System.out.println("CRC32 for 101: "+CRC32.encodeMsg(correctBinaryLength(8,"101")));
+
+        ErrorDetectionAlgorithm Parity = new ParityControl();
+        System.out.println("Parity for ab: "+Parity.encodeMsg(correctBinaryLength(8,stringToBinary("ab"))));
+        System.out.println("Parity for 101: "+Parity.encodeMsg(correctBinaryLength(8,"101")));
     }
 }
